f14743eda828671e2c1522be1138c050f997c674
Olav Vatne <olavvatne@gmail.com>
2024-06-02 11:38:34 +0200

    chore: split out ables from MoveableFluidArea

---
+++ src/components/ables/LabelLeftViewable.jsx
@@ -0,0 +1,35 @@
+const LabelLeftViewable = {
+    name: "LabelLeftViewable",
+    props: {color: String, displayName: String, valueLeft: Number},
+    events: {},
+    render(moveable, React) {
+        const rect = moveable.getRect();
+        const { pos2 } = moveable.state;
+        const transform = `translate(${pos2[0]}px, ${pos2[1]}px) rotate(${rect.rotation}deg)  translate(${-rect.offsetWidth- 30}px, -50px)`
+        return <div key={"label-viewer-left"} 
+            className={"moveable-label-left"} 
+            style={{
+                position: "absolute",
+                left: `0px`,
+                top: `0px`,
+                background: moveable.props.color,
+                borderRadius: "2px",
+                padding: "2px 4px",
+                color: "white",
+                fontSize: "13px",
+                whiteSpace: "nowrap",
+                fontWeight: "bold",
+                willChange: "transform",
+                transform: transform,
+                transformOrigin: "0px 0px"
+        }}>
+            <label style={{display: "block"}}>Start {moveable.props.displayName}</label>
+            <input key={moveable.props.valueLeft} className="moveable-input" 
+                type='tel' pattern="^-?[0-9]\d*\.?\d*$"
+                style={{display: "block", margin: "auto", width: "30px", background: "transparent", color: "white"}} 
+                defaultValue={moveable.props.valueLeft} onBlur={(e) => {moveable.props.onValueLeft(e)}}/>
+        </div>
+    }
+};
+
+export default LabelLeftViewable;
---
+++ src/components/ables/index.js
@@ -0,0 +1,11 @@
+import DimensionViewable from "./DimensionViewable.jsx";
+import EdgeClickable from "./EdgeClickable.jsx";
+import LabelLeftViewable from "./LabelLeftViewable.jsx";
+import LabelRightViewable from "./LabelRightViewable.jsx";
+
+export {
+  DimensionViewable,
+  EdgeClickable,
+  LabelLeftViewable,
+  LabelRightViewable,
+};
