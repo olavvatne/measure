1db40e97a9ff78b89e5113e80a49d3d426cb3040
Olav Vatne <olavvatne@gmail.com>
2024-06-16 14:29:32 +0200

    refactor: move table setup from OverviewPage

--- src/components/table/TableView.jsx
+++ src/components/table/TableView.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { flexRender } from "@tanstack/react-table";
+import "./TableView.css";
 
 export default function TableView({ onRowClick, table }) {
   return (
