4a0cb0187849587f48cfa7a270e9137f5c3b07c1
Olav Vatne <olavvatne@gmail.com>
2024-07-15 16:31:58 +0200

    chore: rename to boundaryArea

--- src/components/editor/MovableLineMeasureArea.jsx
+++ src/components/editor/MovableLineMeasureArea.jsx
@@ -25,7 +25,7 @@ export default function MovableLineMeasureArea({
   color = "red",
   displayName = "O/G",
   disabled = false,
-  measureValues = {
+  boundaryArea = {
     minValue: 5,
     minValue: 10,
     rotation: 0,
@@ -36,7 +36,7 @@ export default function MovableLineMeasureArea({
   },
   value = null,
   setValue = () => {},
-  setMeasureValues = () => {},
+  setBoundaryArea = () => {},
 }) {
   const targetRef = React.useRef(null);
   const moveableRef = React.useRef(null);
@@ -44,37 +44,37 @@ export default function MovableLineMeasureArea({
   React.useEffect(() => {
     if (moveableRef.current) {
       var frame = framer.createFrame(targetRef.current);
-      if (measureValues && measureValues.rotation) {
-        moveableRef.current.moveable.rotation = measureValues.rotation;
-        frame.transform.rotate = measureValues.rotation;
+      if (boundaryArea && boundaryArea.rotation) {
+        moveableRef.current.moveable.rotation = boundaryArea.rotation;
+        frame.transform.rotate = boundaryArea.rotation;
         moveableRef.current.moveable.request(
           "rotatable",
-          { rotate: measureValues.rotation },
+          { rotate: boundaryArea.rotation },
           true
         );
       }
 
       if (
-        measureValues &&
-        measureValues.offsetHeight &&
-        measureValues.offsetWidth
+        boundaryArea &&
+        boundaryArea.offsetHeight &&
+        boundaryArea.offsetWidth
       ) {
-        frame.dimensions.width = measureValues.offsetWidth;
-        frame.dimensions.height = measureValues.offsetHeight;
+        frame.dimensions.width = boundaryArea.offsetWidth;
+        frame.dimensions.height = boundaryArea.offsetHeight;
         moveableRef.current.moveable.request(
           "resizable",
           {
-            offsetWidth: measureValues.offsetWidth,
-            offsetHeight: measureValues.offsetHeight,
+            offsetWidth: boundaryArea.offsetWidth,
+            offsetHeight: boundaryArea.offsetHeight,
           },
           true
         );
       }
 
-      if (measureValues && measureValues.x && measureValues.y) {
+      if (boundaryArea && boundaryArea.x && boundaryArea.y) {
         moveableRef.current.moveable.request(
           "draggable",
-          { x: measureValues.x, y: measureValues.y },
+          { x: boundaryArea.x, y: boundaryArea.y },
           true
         );
       }
@@ -84,10 +84,10 @@ export default function MovableLineMeasureArea({
     }
   }, [targetRef]);
 
-  if (!measureValues || Object.keys(measureValues).length === 0) {
+  if (!boundaryArea || Object.keys(boundaryArea).length === 0) {
     return null;
   }
-  const s = measureValues;
+  const s = boundaryArea;
   const transform = `rotate(${s.rotation || 0}deg)`;
 
   function onFluidEdge(e) {
@@ -96,8 +96,8 @@ export default function MovableLineMeasureArea({
       e.nativeEvent.offsetX,
       padding,
       e.target.offsetWidth,
-      measureValues.minValue,
-      measureValues.maxValue
+      boundaryArea.minValue,
+      boundaryArea.maxValue
     );
     setValue(value);
   }
@@ -108,8 +108,8 @@ export default function MovableLineMeasureArea({
         ref={targetRef}
         style={{
           transform: transform,
-          top: measureValues.y,
-          left: measureValues.x,
+          top: boundaryArea.y,
+          left: boundaryArea.x,
           width: s.offsetWidth + "px",
           height: s.offsetHeight + "px",
         }}
@@ -133,20 +133,20 @@ export default function MovableLineMeasureArea({
           name: name,
           fluidEdge: value,
           displayName: displayName,
-          valueLeft: measureValues.minValue,
-          valueRight: measureValues.maxValue,
+          valueLeft: boundaryArea.minValue,
+          valueRight: boundaryArea.maxValue,
           onValueRight: (e) => {
             if (e.target.validity.valid) {
-              setMeasureValues({
-                ...measureValues,
+              setBoundaryArea({
+                ...boundaryArea,
                 maxValue: parseFloat(e.target.value),
               });
             }
           },
           onValueLeft: (e) => {
             if (e.target.validity.valid) {
-              setMeasureValues({
-                ...measureValues,
+              setBoundaryArea({
+                ...boundaryArea,
                 minValue: parseFloat(e.target.value),
               });
             }
@@ -167,18 +167,18 @@ export default function MovableLineMeasureArea({
         onDragEnd={(e) => {
           const translate = framer.getTranslate(e.target);
 
-          const newX = (measureValues.x || 0) + translate.x;
-          const newY = (measureValues.y || 0) + translate.y;
-          setMeasureValues({ ...measureValues, x: newX, y: newY });
+          const newX = (boundaryArea.x || 0) + translate.x;
+          const newY = (boundaryArea.y || 0) + translate.y;
+          setBoundaryArea({ ...boundaryArea, x: newX, y: newY });
         }}
         onResizeStart={framer.onResizeStart}
         onResize={framer.onResize}
         onResizeEnd={(e) => {
           const translate = framer.getTranslate(e.target);
-          const newX = (measureValues.x || 0) + translate.x;
-          const newY = (measureValues.y || 0) + translate.y;
-          setMeasureValues({
-            ...measureValues,
+          const newX = (boundaryArea.x || 0) + translate.x;
+          const newY = (boundaryArea.y || 0) + translate.y;
+          setBoundaryArea({
+            ...boundaryArea,
             y: newY,
             x: newX,
             offsetHeight: e.lastEvent.offsetHeight,
@@ -188,16 +188,16 @@ export default function MovableLineMeasureArea({
         onRotateStart={framer.onRotateStart}
         onRotate={framer.onRotate}
         onRotateEnd={(e) => {
-          if (!measureValues.rotation) {
-            setMeasureValues({
-              ...measureValues,
+          if (!boundaryArea.rotation) {
+            setBoundaryArea({
+              ...boundaryArea,
               rotation: e.lastEvent.rotate,
             });
           } else if (
-            Math.abs(measureValues.rotation - e.lastEvent.rotate) > 0.001
+            Math.abs(boundaryArea.rotation - e.lastEvent.rotate) > 0.001
           ) {
-            setMeasureValues({
-              ...measureValues,
+            setBoundaryArea({
+              ...boundaryArea,
               rotation: e.lastEvent.rotate,
             });
           }
